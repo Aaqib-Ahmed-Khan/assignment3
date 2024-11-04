a7b7c4f4e3d47a4caed4b2005cf85050cc73dbf8
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2024-11-04 23:44:44 +0500

    first commit

---
+++ aaqib.cpp
@@ -0,0 +1,83 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    float a, b, c, d;
+//    float sum,average,product;
+//
+//cout << "Enter value of a: ";
+//    cin >> a;
+//  cout << "Enter value of b: ";
+//    cin >> b;
+//   cout << "Enter value of c: ";
+//    cin >> c;
+//  cout << "Enter value of d: ";
+//    cin >> d;
+// sum = a + b + c + d;
+//  average = (a + b + c + d) / 4;
+//  product = a * b * c * d;
+//   cout << "Sum of a, b, c, and d is: " << sum << endl;
+//  cout << "Product of a, b, c, and d is: " << product << endl;
+//   cout << "Average of a, b, c, and d is: " << average << endl;
+//
+//    return 0;
+//}
+
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    int rad;
+//   float pi = 3.14159; 
+//   float area; 
+//
+//   cout << "Enter the value of the radius of the circle: ";
+//    cin >> rad;
+//     area = pi * rad * rad;
+//    cout << "The area of the circle is: " <<area<<endl;
+//
+// return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    int years;
+//    int days;  
+// 
+//    cout << "Enter your age in years: ";
+//    cin >> years;   
+//   days = years * 365;
+//    cout << "Your age in days is: " << days << " days." << endl;
+//return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    float c;  
+//    float f;     
+//    cout << "Enter the temperature in Celsius: ";
+//    cin >> c; 
+//
+//    f = (c * 9 / 5) + 32;
+//    cout << "The temperature in Fahrenheit is: " << f << "°F" << endl;
+//   return 0;
+//}
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "Size of char : " << sizeof(char) << endl;
+	cout << "Size of int : " << sizeof(int) << endl;
+	cout << "Size of short int : " << sizeof(short int) << endl;
+	cout << "Size of long int : " << sizeof(long int) << endl;
+	cout << "Size of float : " << sizeof(float) << endl;
+	cout << "Size of double : " << sizeof(double) << endl;
+	cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
+	return 0;
+
+ }
